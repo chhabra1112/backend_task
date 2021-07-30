ac333d9b4fc36d42d9c2dd8e21f20218479bc3cf
Piyush Chhabra <piyushchhabra.1198@gmail.com>
2021-07-30 10:11:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config={
-    mongo_url : "mongodb+srv://admin:iOxwOaoxmk7dsRvn@cluster0.edacg.mongodb.net/getajobdb?retryWrites=true&w=majority",
+    mongo_url : "",
     SECRET: 'mysecretkey'
 }
 
